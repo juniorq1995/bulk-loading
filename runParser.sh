daa6f7213d7382bb1d151fcfd7cfbce8b6ffb9ca
Junior Quintero <juniorq@mac12142.local>
2018-10-04 08:51:33 -0500

    Remove duplicates successful

--- runParser.sh
+++ runParser.sh
@@ -1,1 +1,14 @@
+rm *.dat
 python skeleton_parser.py ebay_data/items-*.json
+
+awk '!seen[$0]++' Item.dat > Item_temp.dat
+mv Item_temp.dat Item.dat
+
+awk '!seen[$0]++' Users.dat > Users_temp.dat
+mv Users_temp.dat Users.dat
+
+awk '!seen[$0]++' Category.dat > Category_temp.dat
+mv Category_temp.dat Category.dat
+
+awk '!seen[$0]++' Bids.dat > Bids_temp.dat
+mv Bids_temp.dat Bids.dat
--- skeleton_parser.py
+++ skeleton_parser.py
@@ -76,14 +76,7 @@ of the necessary SQL tables for your database.
 """
 def parseJson(json_file):
     with open(json_file, 'r') as f:
-        items = loads(f.read())['Items'] # creates a Python dictionary of Items for the supplied json file
-        try:
-            os.remove("Category.dat")
-            os.remove("Item.dat")
-            os.remove("Bids.dat")
-            os.remove("Users.dat")
-        except OSError:
-            pass
+        items = loads(f.read())['Items'] # creates a Python dictionary of Items for the supplied json'!git file
         categoryDataFile = open("Category.dat","a")
         itemDataFile = open("Item.dat","a")
         bidsDataFile = open("Bids.dat", "a")
