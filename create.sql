fbe01c642e2f782ede34f2ff21208ede9c0eaecb
Thomas Mandel <thomasjmandel@hotmail.com>
2018-10-04 22:21:16 -0500

    merged changes

--- create.sql
+++ create.sql
@@ -13,17 +13,25 @@ create table Items (
   Country varchar(255),
   Start varchar(255),
   End varchar(255),
+  Description varchar(255),
+  PRIMARY KEY(ItemID)
 );
 create table Users (
   UserID varchar(255),
   Rating int,
   Location varchar(255),
+  Country varchar(255),
+  PRIMARY KEY(UserID,Location,Country)
 );
 create table Categories (
   Category varchar(255),
+  ItemID int,
+  PRIMARY KEY(Category,ItemID)
 );
 create table Bids (
   UserID varchar(255),
   ItemID int,
   Amount varchar(255),
+  Time varchar(255),
+  PRIMARY KEY(UserID,ItemID,Time)
 );
